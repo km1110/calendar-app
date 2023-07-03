3fd0e155e14f615421f7dccb361daf709c30674f
kohki miyama <kmx.111010@gmail.com>
2023-07-03 16:59:32 +0900

    :sparkles: データベース接続処理の追記

--- backend/golang/app/model/database.go
+++ backend/golang/app/model/database.go
@@ -1,5 +1,45 @@
 package model
 
-import "database/sql"
+import (
+	"database/sql"
+	"fmt"
+	"os"
+)
 
 var Db *sql.DB
+
+func init() {
+	var err error
+
+	dsn := fmt.Sprintf("%s:%s@tcp(db:3306)/%s?charset=utf8", os.Getenv("MYSQL_USER"), os.Getenv("MYSQL_PASSWORD"), os.Getenv("MYSQL_DATABASE"))
+	Db, err = sql.Open("mysql", dsn)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	err = Db.Ping()
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	sql := `CREATE TABLE IF NOT EXISTS schedules(
+			id varchar(26) not null,
+			title text not null,
+			description text not null,
+			date datetime not null,
+			location text not null
+	)`
+
+	_, err = Db.Exec(sql)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println("Connection has been established!")
+}
