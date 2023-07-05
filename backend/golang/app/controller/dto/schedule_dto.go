77b4071bdf6be49ba77465ff998deea19f7a046d
kohki miyama <kmx.111010@gmail.com>
2023-07-05 09:27:05 +0900

    :sparkles: Dateの型をstringからtime.Timeに変更

--- backend/golang/app/controller/dto/schedule_dto.go
+++ backend/golang/app/controller/dto/schedule_dto.go
@@ -1,26 +1,28 @@
 package dto
 
+import "time"
+
 type FeatchScheduleResponse struct {
-	Id          string `json:"id"`
-	Title       string `json:"title"`
-	Description string `json:"description"`
-	Date        string `json:"date"`
-	Location    string `json:"location"`
+	Id          string    `json:"id"`
+	Title       string    `json:"title"`
+	Description string    `json:"description"`
+	Date        time.Time `json:"date"`
+	Location    string    `json:"location"`
 }
 
 type AddScheduleRequest struct {
-	Title       string `json:"title"`
-	Description string `json:"description"`
-	Date        string `json:"date"`
-	Location    string `json:"location"`
+	Title       string    `json:"title"`
+	Description string    `json:"description"`
+	Date        time.Time `json:"date"`
+	Location    string    `json:"location"`
 }
 
 type ChangeScheduleRequest struct {
-	Id          string `json:"id"`
-	Title       string `json:"title"`
-	Description string `json:"description"`
-	Date        string `json:"date"`
-	Location    string `json:"location"`
+	Id          string    `json:"id"`
+	Title       string    `json:"title"`
+	Description string    `json:"description"`
+	Date        time.Time `json:"date"`
+	Location    string    `json:"location"`
 }
 
 type DeleteScheduleRequest struct {
