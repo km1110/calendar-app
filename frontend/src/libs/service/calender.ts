1a0acfb1f4986fa11a03d59a98b71acde314519c
kohki miyama <kmx.111010@gmail.com>
2023-07-08 15:55:09 +0900

    :sparkles: 今月の日付取得処理作成

---
+++ frontend/src/libs/service/calender.ts
@@ -0,0 +1,24 @@
+import dayjs from "dayjs";
+
+interface Date {
+  year: number;
+  month: number;
+}
+
+export const createCalender = (date: Date) => {
+  const firstDay = getMonth(date);
+  const firstDayIndex = firstDay.day();
+
+  return Array(35)
+    .fill(0)
+    .map((_, i) => {
+      const diffFromFirstDay = i - firstDayIndex;
+      const day = firstDay.add(diffFromFirstDay, "day");
+
+      return day;
+    });
+};
+
+export const getMonth = (date: Date) => {
+  return dayjs(`${date.year}-${date.month}`);
+};
