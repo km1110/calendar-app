b8fadb7ebe979b6d9ff9a725026a99d94b432d7c
kohki miyama <kmx.111010@gmail.com>
2023-07-09 21:58:44 +0900

    :sparkles: 月のデータを取得し各日付を展開する

--- frontend/src/components/templates/MonthCalender.tsx
+++ frontend/src/components/templates/MonthCalender.tsx
@@ -1,5 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
+import dayjs from "dayjs";
+
+import { MonthElement } from "@/components/parts/CalenderElement/MonthElement";
+import { createCalender } from "@/libs/service/calender";
 
 export const MonthCalender = () => {
-  return <div>MonthCalender</div>;
+  const currenDate = dayjs();
+  const year = currenDate.year();
+  const month = currenDate.month() + 1;
+
+  const [currentMonth, setCurrentMonth] = useState(
+    createCalender({ year, month })
+  );
+
+  return (
+    <div>
+      {currentMonth.map((days: any, i: number) => (
+        <MonthElement key={i} day={days} />
+      ))}
+    </div>
+  );
 };
