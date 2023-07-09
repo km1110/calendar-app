d599497de31d2acb1867b40b9ffa51e91fde7724
kohki miyama <kmx.111010@gmail.com>
2023-07-09 21:55:45 +0900

    :sparkles: 受け取った日付データを表示する処理

--- frontend/src/components/parts/CalenderElement/MonthElement.tsx
+++ frontend/src/components/parts/CalenderElement/MonthElement.tsx
@@ -1,5 +1,16 @@
+import dayjs from "dayjs";
 import React from "react";
 
-export const MonthElement = () => {
-  return <div>MonthElement</div>;
+export const MonthElement = (props: any) => {
+  const { day, index } = props;
+
+  const today = dayjs();
+
+  return (
+    <div>
+      <header>
+        <span>{day.format("D")}</span>
+      </header>
+    </div>
+  );
 };
