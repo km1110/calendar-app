b2780553d0414d690226b5ac5d262d2ea5a1e399
kohki miyama <kmx.111010@gmail.com>
2023-07-10 16:03:41 +0900

    :adhesive_bandage: ヘッダーの読み込み

--- frontend/src/components/pages/MonthPage.tsx
+++ frontend/src/components/pages/MonthPage.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 
+import { Header } from "../parts/Header";
 import { MonthCalender } from "@/components/templates/MonthCalender";
 
 export const MonthPage = () => {
   return (
     <div>
+      <Header />
       <MonthCalender />
     </div>
   );
