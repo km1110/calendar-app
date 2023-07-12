9c032e7705092ddf1f6eebab346ce0420fe3e577
kohki miyama <kmx.111010@gmail.com>
2023-07-12 20:26:26 +0900

    :sparkles: index.cssをインポート

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "@/App";
 import { CalendarProvider } from "@/provider/CalendarProvider";
+import "@/index.css";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
