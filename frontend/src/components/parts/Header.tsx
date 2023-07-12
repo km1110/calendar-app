faaf2a67b82f3911d0ead50bfc3edfcff84bb2c5
kohki miyama <kmx.111010@gmail.com>
2023-07-12 09:10:55 +0900

    :sparkles: ヘッダーを作成

--- frontend/src/components/parts/Header.tsx
+++ frontend/src/components/parts/Header.tsx
@@ -1,5 +1,47 @@
-import React from "react";
+import {
+  AppBar,
+  Box,
+  Button,
+  IconButton,
+  MenuItem,
+  Toolbar,
+  Typography,
+} from "@mui/material";
 
-export const Header = () => {
-  return <div>Header</div>;
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+import ArrowBackIosNewIcon from "@mui/icons-material/ArrowBackIosNew";
+
+type Props = {
+  setPrevioustMonth: any;
+  setNextMonth: any;
+};
+
+export const Header = ({ setPrevioustMonth, setNextMonth }: Props) => {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuItem />
+          </IconButton>
+          <Typography variant="h6" component="div">
+            Calender
+          </Typography>
+          <IconButton size="small" color="inherit" onClick={setPrevioustMonth}>
+            <ArrowBackIosNewIcon />
+          </IconButton>
+          <IconButton size="small" color="inherit" onClick={setNextMonth}>
+            <ArrowForwardIosIcon />
+          </IconButton>
+          <Button color="inherit">login</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
 };
