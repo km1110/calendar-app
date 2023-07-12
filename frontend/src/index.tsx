9f1012b95eca39d3d120350ff031bff65f73e96b
kohki miyama <kmx.111010@gmail.com>
2023-07-12 09:13:44 +0900

    :sparkles: プロバイダーの設定

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,15 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "@/App";
 import { BrowserRouter } from "react-router-dom";
+
+import App from "@/App";
+import { CalendarProvider } from "@/provider/CalendarProvider";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <CalendarProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </CalendarProvider>
   </React.StrictMode>
 );
