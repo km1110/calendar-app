827a51e2db0df665dc4163d7c4d79fbcc773452b
kohki miyama <kmx.111010@gmail.com>
2023-07-14 08:30:23 +0900

    :sparkles: ダイアログに日付に関する入力フォーマットを作成

--- frontend/src/components/parts/FormDialog.tsx
+++ frontend/src/components/parts/FormDialog.tsx
@@ -2,27 +2,44 @@ import TextField from "@mui/material/TextField";
 import Dialog from "@mui/material/Dialog";
 import DialogActions from "@mui/material/DialogActions";
 import DialogContent from "@mui/material/DialogContent";
-import { Button } from "@mui/material";
+import Button from "@mui/material/Button";
+
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { DatePicker } from "@mui/x-date-pickers";
+
+import AccessTimeIcon from "@mui/icons-material/AccessTime";
+
+import dayjs from "dayjs";
+import { Grid } from "@mui/material";
 
 type Props = {
-  open: boolean;
+  date: dayjs.Dayjs;
   handleOpne: any;
   handleClose: any;
 };
 
-export const FormDialog = ({ open, handleOpne, handleClose }: Props) => {
+const spacer = { margin: "4px 0" };
+
+export const FormDialog = ({ date, handleOpne, handleClose }: Props) => {
   return (
     <div>
       <Dialog open={handleOpne} onClose={handleClose}>
         <DialogContent>
-          <TextField
-            autoFocus
-            margin="dense"
-            id="title"
-            label="タイトル"
-            fullWidth
-            variant="standard"
-          />
+          <Grid
+            container
+            spacing={1}
+            sx={{ alignItems: "center", justifyItems: "space-between" }}
+          >
+            <Grid item>
+              <AccessTimeIcon />
+            </Grid>
+            <Grid item xs={10}>
+              <LocalizationProvider dateAdapter={AdapterDayjs}>
+                <DatePicker value={date} format="YYYY/MM/DD" />
+              </LocalizationProvider>
+            </Grid>
+          </Grid>
         </DialogContent>
         <DialogActions>
           <Button onClick={handleClose}>Cancel</Button>
