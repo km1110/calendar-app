df5d4eefe81b9d5ef04bb6399f2f5d2a3aa08621
kohki miyama <kmx.111010@gmail.com>
2023-07-14 13:17:33 +0900

    :sparkles: タイトル、場所、概要のフォーム追加

--- frontend/src/components/parts/FormDialog.tsx
+++ frontend/src/components/parts/FormDialog.tsx
@@ -3,12 +3,15 @@ import Dialog from "@mui/material/Dialog";
 import DialogActions from "@mui/material/DialogActions";
 import DialogContent from "@mui/material/DialogContent";
 import Button from "@mui/material/Button";
+import Input from "@mui/material/Input";
 
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { DatePicker } from "@mui/x-date-pickers";
 
 import AccessTimeIcon from "@mui/icons-material/AccessTime";
+import LocationOnIcon from "@mui/icons-material/LocationOn";
+import NotesIcon from "@mui/icons-material/Notes";
 
 import dayjs from "dayjs";
 import { Grid } from "@mui/material";
@@ -19,13 +22,21 @@ type Props = {
   handleClose: any;
 };
 
-const spacer = { margin: "4px 0" };
+const spacer = { margin: "10px 0" };
 
 export const FormDialog = ({ date, handleOpne, handleClose }: Props) => {
   return (
     <div>
-      <Dialog open={handleOpne} onClose={handleClose}>
+      <Dialog open={handleOpne} onClose={handleClose} maxWidth="xs" fullWidth>
         <DialogContent>
+          <TextField
+            size="medium"
+            inputProps={{ style: { fontSize: 22 } }}
+            variant="standard"
+            fullWidth
+            placeholder="タイトルを追加"
+            sx={{ marginBottom: 4 }}
+          />
           <Grid
             container
             spacing={1}
@@ -36,8 +47,42 @@ export const FormDialog = ({ date, handleOpne, handleClose }: Props) => {
             </Grid>
             <Grid item xs={10}>
               <LocalizationProvider dateAdapter={AdapterDayjs}>
-                <DatePicker value={date} format="YYYY/MM/DD" />
+                <DatePicker value={date} format="YYYY/MM/DD" className="" />
               </LocalizationProvider>
+            </Grid>
+          </Grid>
+          <Grid
+            container
+            spacing={1}
+            sx={{ alignItems: "center", justifyItems: "space-between" }}
+          >
+            <Grid item>
+              <LocationOnIcon />
+            </Grid>
+            <Grid item xs={10}>
+              <TextField
+                style={spacer}
+                variant="standard"
+                fullWidth
+                placeholder="場所を追加"
+              />
+            </Grid>
+          </Grid>
+          <Grid
+            container
+            spacing={1}
+            sx={{ alignItems: "center", justifyItems: "space-between" }}
+          >
+            <Grid item>
+              <NotesIcon />
+            </Grid>
+            <Grid item xs={10}>
+              <TextField
+                style={spacer}
+                variant="standard"
+                fullWidth
+                placeholder="説明を追加"
+              />
             </Grid>
           </Grid>
         </DialogContent>
