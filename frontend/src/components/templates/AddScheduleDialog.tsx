d8ec8f613ea4d850248cad22fffb809aff01d62b
kohki miyama <kmx.111010@gmail.com>
2023-07-19 20:35:28 +0900

    :sparkles: 各フォームの値に関する状態を作成

--- frontend/src/components/templates/AddScheduleDialog.tsx
+++ frontend/src/components/templates/AddScheduleDialog.tsx
@@ -1,27 +1,40 @@
-import { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { FormDialog } from "../parts/FormDialog";
 import { MonthContext } from "@/provider/CalendarProvider";
+import { scheduleType } from "@/types/schedule";
 
 export const AddScheduleDialog = () => {
-  const { daySelected, setShowDialog } = useContext(MonthContext);
-
-  const handleOpne = () => {
-    setShowDialog(true);
-  };
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [location, setLocation] = useState("");
+  const { daySelected, showDialog, setDaySelected, setShowDialog } =
+    useContext(MonthContext);
 
   const handleClose = () => {
     setShowDialog(false);
   };
 
+  const handleChange: React.ChangeEventHandler<HTMLInputElement> = (event) => {
+    setTitle(event.target.value);
+  };
+
   const handleSaveSchedule = () => {
+    // console.log(setSchedules);
     setShowDialog(false);
   };
 
   return (
     <div>
       <FormDialog
+        title={title}
+        description={description}
+        location={location}
         date={daySelected}
-        handleOpne={handleOpne}
+        setTitle={setTitle}
+        setDescription={setDescription}
+        setLocation={setLocation}
+        setDaySelected={setDaySelected}
+        showDialog={showDialog}
         handleClose={handleClose}
         handleSaveSchedule={handleSaveSchedule}
       />
