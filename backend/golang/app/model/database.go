fa95be3e0893456d8e4b54530f3a1f40c69f08fc
kohki miyama <kmx.111010@gmail.com>
2023-07-20 19:13:29 +0900

    :sparkles: DATETIME型をtime.Time型のフィールドで読み込めるようにした。

--- backend/golang/app/model/database.go
+++ backend/golang/app/model/database.go
@@ -13,7 +13,7 @@ var Db *sql.DB
 func init() {
 	var err error
 
-	dsn := fmt.Sprintf("%s:%s@tcp(db:3306)/%s?charset=utf8", os.Getenv("MYSQL_USER"), os.Getenv("MYSQL_PASSWORD"), os.Getenv("MYSQL_DATABASE"))
+	dsn := fmt.Sprintf("%s:%s@tcp(db:3306)/%s?charset=utf8&parseTime=true", os.Getenv("MYSQL_USER"), os.Getenv("MYSQL_PASSWORD"), os.Getenv("MYSQL_DATABASE"))
 	Db, err = sql.Open("mysql", dsn)
 
 	if err != nil {
