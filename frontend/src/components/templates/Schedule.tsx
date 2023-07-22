482b778a5937e26006364db3297da78ca9f92588
kohki miyama <kmx.111010@gmail.com>
2023-07-22 09:57:07 +0900

    :zap: スケジュール表示のスタイルを変更

--- frontend/src/components/templates/Schedule.tsx
+++ frontend/src/components/templates/Schedule.tsx
@@ -7,5 +7,12 @@ type Props = {
 };
 
 export const Schedule = ({ schedule, handleChangeSchedule }: Props) => {
-  return <div onClick={handleChangeSchedule}>{schedule.title}</div>;
+  return (
+    <div
+      className={`bg-neutral-200 p-1 mr-3 text-gray-600 text-sm rounded mb-1 truncate`}
+      onClick={handleChangeSchedule}
+    >
+      {schedule.title}
+    </div>
+  );
 };
