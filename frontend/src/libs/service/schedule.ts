f559b1fb9dccb756c0e9515dd42a1114871c7cf8
kohki miyama <kmx.111010@gmail.com>
2023-07-22 09:51:00 +0900

    :zap: string型をdayjs型に変換する処理の追加

--- frontend/src/libs/service/schedule.ts
+++ frontend/src/libs/service/schedule.ts
@@ -1,7 +1,9 @@
 import { isSameDay } from "@/libs/service/calender";
+import dayjs from "dayjs";
 
-export const setSchedules = (calendar: any, schedules: any) =>
-  calendar.map((c: any) => ({
+export const setSchedules = (calendar: any, schedules: any) => {
+  return calendar.map((c: any) => ({
     date: c,
-    schedules: schedules.fillter((e: any) => isSameDay(e.date, c)),
+    schedules: schedules.filter((e: any) => isSameDay(dayjs(e.date), c)),
   }));
+};
