02cebe380b8b60f44034645e8bd6e10ca4284251
kohki miyama <kmx.111010@gmail.com>
2023-07-25 09:29:03 +0900

    :zap: スケジュール表示ダイアログの雛形を作成

--- frontend/src/components/parts/CurrentDialog.tsx
+++ frontend/src/components/parts/CurrentDialog.tsx
@@ -1,5 +1,54 @@
-import React from "react";
+import Grid from "@mui/material/Grid";
+import TextField from "@mui/material/TextField";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
 
-export const CurrentDialog = () => {
-  return <div>CurrentDialog</div>;
+import CloseIcon from "@mui/icons-material/Close";
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteIcon from "@mui/icons-material/Delete";
+
+import { scheduleType } from "@/types/schedule";
+import { DialogContentText, DialogTitle } from "@mui/material";
+
+type Props = {
+  schedule: scheduleType;
+  showDialog: boolean;
+  handleChange: any;
+  handleDelete: any;
+  handleClose: any;
+};
+
+export const CurrentDialog = ({
+  schedule,
+  showDialog,
+  handleChange,
+  handleDelete,
+  handleClose,
+}: Props) => {
+  return (
+    <div>
+      <Dialog open={showDialog} onClose={handleClose}>
+        <DialogActions>
+          <IconButton onClick={handleChange}>
+            <EditIcon />
+          </IconButton>
+          <IconButton onClick={handleDelete}>
+            <DeleteIcon />
+          </IconButton>
+          <IconButton onClick={handleClose}>
+            <CloseIcon />
+          </IconButton>
+        </DialogActions>
+        <DialogTitle>{schedule.title}</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            {schedule.date.format("MM月DD日")}
+          </DialogContentText>
+        </DialogContent>
+      </Dialog>
+    </div>
+  );
 };
