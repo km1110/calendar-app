3fc8defb352434a555a18a08a04c4018f72602a3
kohki miyama <kmx.111010@gmail.com>
2023-07-25 09:26:54 +0900

    :zap: 初回レンダリング時にデータベース上の情報を取得

--- frontend/src/components/views/MonthView.tsx
+++ frontend/src/components/views/MonthView.tsx
@@ -1,13 +1,30 @@
+import { useContext, useEffect } from "react";
+
 import { HeaderTemplate } from "@/components/templates/HeaderTemplate";
 import { MonthCalender } from "@/components/templates/MonthCalender";
 import { AddScheduleDialog } from "@/components/templates/AddScheduleDialog";
+import { CurrentScheduleDialog } from "@/components/templates/CurrentScheduleDialog";
+import { MonthContext } from "@/provider/CalendarProvider";
+import { client } from "@/libs/api/axios";
 
 export const MonthView = () => {
+  const { setSchedules } = useContext(MonthContext);
+
+  useEffect(() => {
+    const getSchedules = async () => {
+      client.get("schedule/fetch-schedules").then(({ data }) => {
+        setSchedules(data);
+      });
+    };
+    getSchedules();
+  }, []);
+
   return (
     <div>
       <HeaderTemplate />
       <MonthCalender />
       <AddScheduleDialog />
+      <CurrentScheduleDialog />
     </div>
   );
 };
