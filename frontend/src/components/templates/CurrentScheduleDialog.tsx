7d1095f3d008840aae2fd00ba3cdfe347899769d
kohki miyama <kmx.111010@gmail.com>
2023-07-26 09:55:06 +0900

    :zap: スケジュール変更、削除のハンドルを追加

--- frontend/src/components/templates/CurrentScheduleDialog.tsx
+++ frontend/src/components/templates/CurrentScheduleDialog.tsx
@@ -1,5 +1,40 @@
-import React from "react";
+import React, { useContext } from "react";
+
+import { CurrentDialog } from "@/components/parts/CurrentDialog";
+import { MonthContext } from "@/provider/CalendarProvider";
+import { client } from "@/libs/api/axios";
+import { setSchedules } from "@/libs/service/schedule";
 
 export const CurrentScheduleDialog = () => {
-  return <div>CurrentScheduleDialog</div>;
+  const { schedule, showDialog, setShowDialog, setSchedules } =
+    useContext(MonthContext);
+
+  const handleChange = (e: any) => {
+    // 他のイベントが発火するのをキャンセル
+    e.stopPropagation();
+  };
+
+  const handleDelete = async (id: string) => {
+    await client.post("schedule/delete-schedule", { id });
+    client.get("schdule/fetch-schedules").then(({ data }) => {
+      setSchedules(data);
+    });
+    setShowDialog(false);
+  };
+
+  const handleClose = () => {
+    setShowDialog(false);
+  };
+
+  return (
+    <div>
+      <CurrentDialog
+        schedule={schedule}
+        showDialog={showDialog}
+        handleChange={handleChange}
+        handleDelete={handleDelete}
+        handleClose={handleClose}
+      />
+    </div>
+  );
 };
