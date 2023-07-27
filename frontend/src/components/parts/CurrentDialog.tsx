f7e6d186b90a7bd3a7cc46bd430e8e0a975f6d73
kohki miyama <kmx.111010@gmail.com>
2023-07-27 17:01:39 +0900

    :adhesive_bandage: 不要なコメントアウトを削除

--- frontend/src/components/parts/CurrentDialog.tsx
+++ frontend/src/components/parts/CurrentDialog.tsx
@@ -44,7 +44,6 @@ export const CurrentDialog = ({
             onClick={(e) => {
               setShowDialog(false);
               setShowChangeDialog(true);
-              // handleChange(schedule);
             }}
           >
             <EditIcon />
