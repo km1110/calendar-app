7581c37cb5cdfac1930bfc09cdd0a4b5e88bbeb7
kohki miyama <kmx.111010@gmail.com>
2023-07-27 15:42:17 +0900

    :zap: スケジュール変更に関するダイアログを追加 #10

--- frontend/src/components/views/MonthView.tsx
+++ frontend/src/components/views/MonthView.tsx
@@ -4,6 +4,7 @@ import { HeaderTemplate } from "@/components/templates/HeaderTemplate";
 import { MonthCalender } from "@/components/templates/MonthCalender";
 import { AddScheduleDialog } from "@/components/templates/AddScheduleDialog";
 import { CurrentScheduleDialog } from "@/components/templates/CurrentScheduleDialog";
+import { ChangeScheduleDialog } from "../templates/ChangeScheduleDialog";
 import { MonthContext } from "@/provider/CalendarProvider";
 import { client } from "@/libs/api/axios";
 
@@ -26,6 +27,7 @@ export const MonthView = () => {
       <MonthCalender />
       <AddScheduleDialog />
       <CurrentScheduleDialog />
+      <ChangeScheduleDialog />
     </div>
   );
 };
