a1769d4b197d58dc8466988c9527913403447cbc
kohki miyama <kmx.111010@gmail.com>
2023-08-07 12:03:40 +0900

    :fire: DB migrate 処理を削除

--- backend/golang/app/cmd/main.go
+++ backend/golang/app/cmd/main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"flag"
-	"fmt"
 	"net/http"
 
 	"github.com/km1110/calendar-app/backend/golang/controller"
@@ -13,25 +11,7 @@ var sm = model.CreateScheduleModel()
 var sc = controller.CreateScheduleController(sm)
 var ro = controller.CreateRouter(sc)
 
-func migrate() {
-	sql := `INSERT INTO schedules(id, title, date, description, location) VALUES('00000000000000000000000000', 'dummy', '20230701', 'test', 'home');`
-
-	_, err := model.Db.Exec(sql)
-
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	fmt.Println("Migration is success!")
-}
-
 func main() {
-	f := flag.String("option", "", "migrate database or not")
-	flag.Parse()
-	if *f == "migrate" {
-		migrate()
-	}
 	ro.HandleRequest()
 	http.ListenAndServe(":8080", nil)
 }
