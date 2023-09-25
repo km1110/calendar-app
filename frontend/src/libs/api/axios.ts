b76b451336fe2cdec3311f7715bf08fce6a14426
kohki miyama <kmx.111010@gmail.com>
2023-09-25 20:47:50 +0900

    不要な記述を削除

--- frontend/src/libs/api/axios.ts
+++ frontend/src/libs/api/axios.ts
@@ -1,11 +1,5 @@
 import axios from "axios";
 
-axios.defaults.withCredentials = true;
-
 export const client = axios.create({
   baseURL: "http://localhost:8080",
-  headers: {
-    // "Content-Type": "application/json",
-    "Access-Control-Allow-Origin": "http://localhost:3000",
-  },
 });
