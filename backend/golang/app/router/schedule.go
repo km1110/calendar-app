17eb9509a03ab72bf2a45dc458bd95728fb2ca34
kohki miyama <kmx.111010@gmail.com>
2023-09-25 20:47:32 +0900

    putとdeleteのコントローラーの指定

--- backend/golang/app/router/schedule.go
+++ backend/golang/app/router/schedule.go
@@ -9,6 +9,6 @@ func initScheduleRouter(router *gin.Engine) {
 	s := router.Group("/schedule")
 	s.GET("", controller.FeatchSchedule)
 	s.POST("", controller.AddSchedule)
-	s.PUT("/:schedule_id")
-	s.DELETE("/:schedule_id")
+	s.PUT("/:schedule_id", controller.UpdateSchedule)
+	s.DELETE("/:schedule_id", controller.DeleteSchedule)
 }
