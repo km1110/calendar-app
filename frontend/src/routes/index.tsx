588aa420533c75cdb6d039f90dcc0909105a7e24
kohki miyama <kmx.111010@gmail.com>
2023-09-26 10:57:36 +0900

    サインアップに関するルーティングの追加(#26)

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -2,11 +2,15 @@ import React from "react";
 import { Routes, Route } from "react-router";
 
 import { MonthPage } from "@/components/pages/MonthPage";
+import StartPage from "@/components/pages/StartPage";
+import { SignUpPage } from "@/components/pages/SignUpPage";
 
 export const AppRouter = () => {
   return (
     <Routes>
-      <Route path="/" element={<MonthPage />}></Route>
+      <Route path="/" element={<SignUpPage />}></Route>
+      <Route path="/start" element={<StartPage />}></Route>
+      <Route path="/calendar" element={<MonthPage />}></Route>
     </Routes>
   );
 };
