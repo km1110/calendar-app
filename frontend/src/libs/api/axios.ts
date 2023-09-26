098cb0768137c1fe715bdd6c0be95788b92b427c
kohki miyama <kmx.111010@gmail.com>
2023-09-26 19:44:02 +0900

    makeInstanceの作成(#26)

--- frontend/src/libs/api/axios.ts
+++ frontend/src/libs/api/axios.ts
@@ -1,5 +1,21 @@
-import axios from "axios";
+import React from "react";
 
-export const client = axios.create({
-  baseURL: "http://localhost:8080",
-});
+import axios from "axios";
+import { getAuth } from "firebase/auth";
+import { app } from "@/libs/firebase";
+
+export const makeIntance = () => {
+  const instance = axios.create({
+    baseURL: "http://localhost:8080",
+  });
+  // const user = useFirebaseUser();
+  const auth = getAuth(app);
+  instance.interceptors.request.use(async (request) => {
+    const token = await auth.currentUser?.getIdToken();
+    console.log(token);
+    request.headers.common["Authorization"] = `bearer ${token}`;
+    return request;
+  });
+
+  return instance;
+};
