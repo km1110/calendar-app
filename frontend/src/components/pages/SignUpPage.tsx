de266b8a064cdf3109b2aca3fd13023ac10627da
kohki miyama <kmx.111010@gmail.com>
2023-09-26 10:56:09 +0900

    サインアップのページを実装(#26)

---
+++ frontend/src/components/pages/SignUpPage.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { SignUpView } from "../views/SignUpView";
+
+export const SignUpPage = () => {
+  return (
+    <div>
+      <SignUpView />
+    </div>
+  );
+};
