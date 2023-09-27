baaf7ebd532f6f6feb9888d1682a3fdc99b176a0
kohki miyama <kmx.111010@gmail.com>
2023-09-28 08:50:57 +0900

    サインイン、サインアップそれぞれの型を作成(#26)

--- frontend/src/types/sign.d.ts
+++ frontend/src/types/sign.d.ts
@@ -1,5 +1,10 @@
-export interface signType {
+export interface signupType {
   name: string;
+  email: string;
+  password: string;
+}
+
+export interface signinType {
   email: string;
   password: string;
 }
