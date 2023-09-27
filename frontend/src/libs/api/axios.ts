38abef30ecd0fdf82c66016d7a1bd92fa3776816
kohki miyama <kmx.111010@gmail.com>
2023-09-28 08:51:39 +0900

    token取得に関する例外処理の記述を追加(#26)

--- frontend/src/libs/api/axios.ts
+++ frontend/src/libs/api/axios.ts
@@ -7,13 +7,17 @@ import { app } from "@/libs/firebase";
 export const makeIntance = () => {
   const instance = axios.create({
     baseURL: "http://localhost:8080",
+    timeout: 15000,
   });
   // const user = useFirebaseUser();
   const auth = getAuth(app);
   instance.interceptors.request.use(async (request) => {
     const token = await auth.currentUser?.getIdToken();
-    console.log(token);
-    request.headers.common["Authorization"] = `bearer ${token}`;
+    if (token) {
+      request.headers.Authorization = `${token}`;
+    } else {
+      console.log("No token");
+    }
     return request;
   });
 
