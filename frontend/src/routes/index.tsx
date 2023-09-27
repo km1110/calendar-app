068fbbd325f08b9f1ffa3994c62781159ef3fb8c
kohki miyama <kmx.111010@gmail.com>
2023-09-28 08:53:11 +0900

    サインインのルーターを設定(#26)

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -4,11 +4,13 @@ import { Routes, Route } from "react-router";
 import { MonthPage } from "@/components/pages/MonthPage";
 import StartPage from "@/components/pages/StartPage";
 import { SignUpPage } from "@/components/pages/SignUpPage";
+import { SignInPage } from "@/components/pages/SignInPage";
 
 export const AppRouter = () => {
   return (
     <Routes>
       <Route path="/" element={<SignUpPage />}></Route>
+      <Route path="/signin" element={<SignInPage />}></Route>
       <Route path="/start" element={<StartPage />}></Route>
       <Route path="/calendar" element={<MonthPage />}></Route>
     </Routes>
