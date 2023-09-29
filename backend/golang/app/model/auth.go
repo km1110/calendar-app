90cd139fec6699cead3c265e9655d4e705c06a43
kohki miyama <kmx.111010@gmail.com>
2023-09-29 10:43:59 +0900

    auth modelの作成(#26)

--- backend/golang/app/model/auth.go
+++ backend/golang/app/model/auth.go
@@ -1,1 +1,36 @@
 package model
+
+import (
+	"context"
+	"math/rand"
+	"time"
+
+	"github.com/km1110/calendar-app/backend/golang/model/entities"
+	"github.com/oklog/ulid"
+)
+
+type UserModel struct {
+}
+
+func NEwUserModel() *UserModel {
+	return &UserModel{}
+}
+
+func (um *UserModel) AddUser(ctx context.Context, r entities.SignUp) error {
+	t := time.Now()
+	entropy := ulid.Monotonic(rand.New(rand.NewSource(t.UnixMicro())), 0)
+	id := ulid.MustNew(ulid.Timestamp(t), entropy)
+
+	req := entities.SignUp{
+		Id:           id.String(),
+		Firebase_uid: r.Firebase_uid,
+		Username:     r.Username,
+		Email:        r.Email,
+	}
+
+	sql := `INSERT INTO users(id, firebase_uid, username, email) VALUES(?, ?, ?, ?)`
+
+	_, err := Db.Exec(sql, req.Id, req.Firebase_uid, req.Username, req.Email)
+
+	return err
+}
