ac75f95ba305634ccf8b16b0211b03cf3eaf7aa3
kohki miyama <kmx.111010@gmail.com>
2023-09-30 11:53:33 +0900

    サインインに関する処理の追加(#26)

--- backend/golang/app/controller/auth.go
+++ backend/golang/app/controller/auth.go
@@ -1,11 +1,19 @@
 package controller
 
 import (
+	"fmt"
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 	"github.com/km1110/calendar-app/backend/golang/controller/dto"
+	"github.com/km1110/calendar-app/backend/golang/model"
+	"github.com/km1110/calendar-app/backend/golang/model/entities"
 )
 
 func CreateUser(c *gin.Context) {
+	uid, _ := c.Get("firebaseUID")
+	firebase_uid := uid.(string)
+
 	var req dto.CreateUserRequest
 
 	if err := c.Bind(&req); err != nil {
@@ -13,4 +21,25 @@ func CreateUser(c *gin.Context) {
 			"error": err.Error(),
 		})
 	}
+
+	um := model.NewUserModel()
+
+	if err := um.AddUser(c, entities.SignUp{
+		Firebase_uid: firebase_uid,
+		Username:     req.Name,
+		Email:        req.Email,
+	}); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "user created successfully"})
+}
+
+func GetUser(c *gin.Context) {
+	fmt.Println("test1")
+	uid, _ := c.Get("firebaseUID")
+	firebase_uid := uid.(string)
+
+	fmt.Println("firebase_uid: ", firebase_uid)
 }
