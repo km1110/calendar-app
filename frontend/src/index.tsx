feaebcc0b2a747826e68df3f4327620557d53df7
kohki miyama <kmx.111010@gmail.com>
2023-10-01 13:34:24 +0900

    ルーターの呼び出し位置を変更(#26)

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -4,16 +4,19 @@ import { BrowserRouter } from "react-router-dom";
 import App from "@/App";
 import { CalendarProvider } from "@/provider/CalendarProvider";
 import "@/index.css";
+import { FirebaseAuthProvider } from "./provider/FirebaseAuthProvider";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <CalendarProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </CalendarProvider>
+    <BrowserRouter>
+      <FirebaseAuthProvider>
+        <CalendarProvider>
+          <App />
+        </CalendarProvider>
+      </FirebaseAuthProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
