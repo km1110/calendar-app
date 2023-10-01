5e96003bcc7ac18591325f421b603945f919ca91
kohki miyama <kmx.111010@gmail.com>
2023-10-01 13:33:57 +0900

    不要なリンクを削除(#26)

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { Routes, Route } from "react-router";
 
 import { MonthPage } from "@/components/pages/MonthPage";
-import StartPage from "@/components/pages/StartPage";
 import { SignUpPage } from "@/components/pages/SignUpPage";
 import { SignInPage } from "@/components/pages/SignInPage";
 
@@ -11,7 +10,6 @@ export const AppRouter = () => {
     <Routes>
       <Route path="/" element={<SignUpPage />}></Route>
       <Route path="/signin" element={<SignInPage />}></Route>
-      <Route path="/start" element={<StartPage />}></Route>
       <Route path="/calendar" element={<MonthPage />}></Route>
     </Routes>
   );
