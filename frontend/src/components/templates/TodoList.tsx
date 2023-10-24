a813cb04126e32b1c804822d6332e041a8e694b2
kohki miyama <kmx.111010@gmail.com>
2023-10-25 07:18:46 +0900

    cssの追加(#28)

--- frontend/src/components/templates/TodoList.tsx
+++ frontend/src/components/templates/TodoList.tsx
@@ -16,21 +16,25 @@ export const TodoList = ({ todos }: Props) => {
   return (
     <Box
       marginTop="20px"
-      height="100%"
+      width="100%"
+      height="30%"
       display="flex"
       flexDirection="column"
       alignItems="center"
     >
-      <Card variant="outlined" sx={{ width: "80%", border: "1px solid" }}>
+      <Card
+        variant="outlined"
+        sx={{
+          width: "80%",
+          border: "1px solid",
+        }}
+      >
         <Box
           display="flex"
           flexDirection="row"
           sx={{ borderBottom: "1px solid" }}
         >
-          <Typography
-            variant="h5"
-            sx={{ flexGrow: 1, marginTop: "1%", marginLeft: "2%" }}
-          >
+          <Typography sx={{ flexGrow: 1, marginTop: "1%", marginLeft: "2%" }}>
             TODO
           </Typography>
           <Button onClick={() => setIsOpen(true)}>追加</Button>
@@ -39,60 +43,61 @@ export const TodoList = ({ todos }: Props) => {
         <Box sx={{ borderBottom: "1px solid" }}>
           <Grid container>
             <Grid item xs={1}>
-              <Typography variant="h6" sx={{ marginLeft: "10px" }}>
+              <Typography sx={{ fontSize: "18px", marginLeft: "10px" }}>
                 完了
               </Typography>
             </Grid>
             <Grid item xs={3}>
-              <Typography variant="h6">タスク名</Typography>
+              <Typography sx={{ fontSize: "18px" }}>タスク名</Typography>
             </Grid>
             <Grid item xs={2}>
-              <Typography variant="h6">カテゴリー</Typography>
+              <Typography sx={{ fontSize: "18px" }}>カテゴリー</Typography>
             </Grid>
             <Grid item xs={2}>
-              <Typography variant="h6">日付</Typography>
+              <Typography sx={{ fontSize: "18px" }}>日付</Typography>
             </Grid>
             <Grid item xs={3}>
-              <Typography variant="h6">プロジェクト名</Typography>
+              <Typography sx={{ fontSize: "18px" }}>プロジェクト名</Typography>
             </Grid>
           </Grid>
         </Box>
-        {todos.map((item: todoType, index: number) => (
-          <Grid
-            container
-            // spacing={3}
-            key={index}
-            alignItems="center"
-            style={{ minHeight: "40px" }}
-            onMouseEnter={() => setHoveredIndex(index)}
-            onMouseLeave={() => setHoveredIndex(null)}
-          >
-            <Grid item xs={1}>
-              <Typography sx={{ marginLeft: "10px" }}>
-                <CheckBox />
-              </Typography>
+        <Box sx={{ overflowY: "auto", minHeight: "calc(40px * 10)" }}>
+          {todos.map((item: todoType, index: number) => (
+            <Grid
+              container
+              key={index}
+              alignItems="center"
+              style={{ minHeight: "40px" }}
+              onMouseEnter={() => setHoveredIndex(index)}
+              onMouseLeave={() => setHoveredIndex(null)}
+            >
+              <Grid item xs={1}>
+                <Typography sx={{ marginLeft: "10px" }}>
+                  <CheckBox />
+                </Typography>
+              </Grid>
+              <Grid item xs={3}>
+                <Typography>{item.name}</Typography>
+              </Grid>
+              <Grid item xs={2}>
+                <Typography>{item.tag}</Typography>
+              </Grid>
+              <Grid item xs={2}>
+                <Typography>2023/10/19</Typography>
+              </Grid>
+              <Grid item xs={3}>
+                <Typography>{item.project}</Typography>
+              </Grid>
+              <Grid item xs={1}>
+                {hoveredIndex === index && (
+                  <IconButton>
+                    <Edit />
+                  </IconButton>
+                )}
+              </Grid>
             </Grid>
-            <Grid item xs={3}>
-              <Typography>{item.name}</Typography>
-            </Grid>
-            <Grid item xs={2}>
-              <Typography>{item.tag}</Typography>
-            </Grid>
-            <Grid item xs={2}>
-              <Typography>2023/10/19</Typography>
-            </Grid>
-            <Grid item xs={3}>
-              <Typography>{item.project}</Typography>
-            </Grid>
-            <Grid item xs={1}>
-              {hoveredIndex === index && (
-                <IconButton>
-                  <Edit />
-                </IconButton>
-              )}
-            </Grid>
-          </Grid>
-        ))}
+          ))}
+        </Box>
       </Card>
     </Box>
   );
