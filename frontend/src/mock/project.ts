a069d3bdffbcd067f6e90a283eed6ecbccfc0a0a
kohki miyama <kmx.111010@gmail.com>
2023-10-25 20:15:08 +0900

    feat: :sparkles: projectの型を作成(#28)

---
+++ frontend/src/mock/project.ts
@@ -0,0 +1,22 @@
+import { projectType } from "@/types/project";
+
+export const initialProjects: projectType[] = [
+  {
+    id: "1",
+    title: "project 1",
+    description: "hogehoge",
+    num: 3,
+  },
+  {
+    id: "2",
+    title: "project 2",
+    description: "hogehoge",
+    num: 5,
+  },
+  {
+    id: "3",
+    title: "project 3",
+    description: "hogehoge",
+    num: 6,
+  },
+];
