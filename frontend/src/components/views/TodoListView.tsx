8e409bec6b22dbeffb532602d27cc8826216bfca
kohki miyama <kmx.111010@gmail.com>
2023-10-25 22:00:36 +0900

    feat: :zap: 定義を追加(#28)

--- frontend/src/components/views/TodoListView.tsx
+++ frontend/src/components/views/TodoListView.tsx
@@ -1,11 +1,79 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+
+import dayjs from "dayjs";
+
+import { makeIntance } from "@/libs/api/axios";
+import { todoType } from "@/types/todo";
+
 import { TodoList } from "../templates/TodoList";
+import { Box } from "@mui/material";
+import { ProjectList } from "../templates/ProjectList";
+import { RoutineList } from "../templates/RoutineList";
+import { projectType } from "@/types/project";
+import { initialTodos } from "@/mock/todo";
+import { initialProjects } from "@/mock/project";
 
 export const TodoListView = () => {
-  const [todos, setTodos] = useState(["hoge"]);
+  const [todos, setTodos] = useState<todoType[]>(initialTodos);
+  const [projects, setProjects] = useState<projectType[]>(initialProjects);
+  const [routines, setRoutines] = useState([]);
+
+  const [todo, setTodo] = useState<todoType>({
+    id: "",
+    name: "",
+    tag: "",
+    date: dayjs(),
+    project: "",
+    status: false,
+  });
+
+  const instance = makeIntance();
+
+  // useEffect(() => {
+  //   const getTodoLists = async () => {
+  //     instance
+  //       .get("/todos")
+  //       .then(({ data }) => {
+  //         setTodos(data);
+  //       })
+  //       .catch((error) => {
+  //         console.error("An error occurred while fetching the todos:", error);
+  //       });
+  //   };
+  //   getTodoLists();
+  // }, []);
+
+  const handleSubmit = async () => {
+    const body = {
+      name: todo.name,
+      tag: todo.tag,
+      date: todo.date.toISOString(),
+      project: todo.project,
+      status: todo.status,
+    };
+    await instance.post("/todos", body);
+    instance.get("/todos").then(({ data }) => {
+      setTodos(data);
+    });
+  };
+
+  const handleDelete = async () => {};
+
   return (
-    <div>
-      <TodoList todos={todos} />
-    </div>
+    <Box display="flex" flexDirection="column" alignItems="center" width="100%">
+      <Box width="100%" height="50%">
+        <TodoList todos={todos} setTodo={setTodo} />
+      </Box>
+      <Box
+        display="flex"
+        flexDirection="row"
+        alignItems="center"
+        gap="20px"
+        width="100%"
+      >
+        {/* <ProjectList projects={projects} handleDelete={handleDelete} />
+        <RoutineList routines={routines} /> */}
+      </Box>
+    </Box>
   );
 };
