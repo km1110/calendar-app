826216fe50090843b5e4331277a2a5e34825175b
kohki miyama <kmx.111010@gmail.com>
2023-10-25 20:17:24 +0900

    feat: :zap: 編集にも対応(#28)

--- frontend/src/components/templates/AddTodoDialog.tsx
+++ frontend/src/components/templates/AddTodoDialog.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-import { TodoDialog } from "../parts/TodoDialog";
 import {
   Dialog,
   DialogActions,
@@ -11,37 +9,26 @@ import {
   Button,
   MenuItem,
 } from "@mui/material";
-import { LocalizationProvider } from "@mui/x-date-pickers";
+import { DatePicker, LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
-// import { ja } from "date-fns/locale";
 
 import CloseIcon from "@mui/icons-material/Close";
+import { todoType } from "@/types/todo";
+import { currencies } from "@/mock/tag";
 
 type Props = {
   isOpen: boolean;
-  onClose: any;
+  onClose: () => void;
+  handleChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  selectedItem: todoType | null;
 };
 
-export const AddTodoDialog = ({ isOpen, onClose }: Props) => {
-  const currencies = [
-    {
-      value: "schoole",
-      label: "学校",
-    },
-    {
-      value: "job",
-      label: "仕事",
-    },
-    {
-      value: "hobby",
-      label: "趣味",
-    },
-    {
-      value: "play",
-      label: "遊び",
-    },
-  ];
-
+export const AddTodoDialog = ({
+  isOpen,
+  onClose,
+  handleChange,
+  selectedItem,
+}: Props) => {
   return (
     <Dialog open={isOpen} onClose={onClose} maxWidth="xs">
       <DialogActions>
@@ -51,32 +38,30 @@ export const AddTodoDialog = ({ isOpen, onClose }: Props) => {
       </DialogActions>
       <DialogContent>
         <Typography>タスク名</Typography>
-        <LocalizationProvider dateAdapter={AdapterDateFns}>
-          <TextField
-            size="medium"
-            defaultValue=""
-            id="name"
-            name="name"
-            variant="outlined"
-            type="text"
-            sx={{
-              width: { xs: "200px", sm: "360px" },
-              background: "white",
-              marginBottom: { xs: "20px" },
-            }}
-            // onChange={handleChange}
-          />
-        </LocalizationProvider>
+        <TextField
+          size="medium"
+          defaultValue={selectedItem ? selectedItem.name : ""}
+          id="name"
+          name="name"
+          variant="outlined"
+          type="text"
+          sx={{
+            width: { xs: "200px", sm: "360px" },
+            background: "white",
+            marginBottom: { xs: "20px" },
+          }}
+          onChange={handleChange}
+        />
         <Typography>カテゴリー</Typography>
         <TextField
           select
           size="medium"
-          defaultValue=""
+          defaultValue={selectedItem ? selectedItem.tag : ""}
           id="tag"
           name="tag"
           variant="outlined"
           sx={{ width: { xs: "200px", sm: "360px" }, marginBottom: "20px" }}
-          // onChange={handleChange}
+          onChange={handleChange}
         >
           {currencies.map((option) => (
             <MenuItem key={option.value} value={option.value}>
@@ -85,16 +70,22 @@ export const AddTodoDialog = ({ isOpen, onClose }: Props) => {
           ))}
         </TextField>
         <Typography>日付</Typography>
-        <TextField
-          size="medium"
-          defaultValue=""
-          id="ends_time"
-          name="ends_time"
-          variant="outlined"
-          type="datetime-local"
-          sx={{ width: { xs: "200px", sm: "360px" }, marginBottom: "20px" }}
-          // onChange={handleChange}
-        />
+        <LocalizationProvider dateAdapter={AdapterDateFns}>
+          <TextField
+            size="medium"
+            defaultValue={selectedItem ? selectedItem.date : ""}
+            id="date"
+            name="date"
+            variant="outlined"
+            type="date"
+            sx={{
+              width: { xs: "200px", sm: "360px" },
+              background: "white",
+              marginBottom: { xs: "20px" },
+            }}
+            onChange={handleChange}
+          />
+        </LocalizationProvider>
       </DialogContent>
       <DialogActions>
         <Box display="flex" flexDirection="column" alignItems="center">
