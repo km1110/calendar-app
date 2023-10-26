4a6a4e688e949fb6fcb68086fe5b1a7973ab0fe7
kohki miyama <kmx.111010@gmail.com>
2023-10-26 21:48:54 +0900

    refactor: :pencil2: fatchのタイポ修正

--- backend/golang/app/router/schedule.go
+++ backend/golang/app/router/schedule.go
@@ -7,7 +7,7 @@ import (
 
 func initScheduleRouter(router *gin.RouterGroup) {
 	s := router.Group("/schedule")
-	s.GET("", controller.FeatchSchedule)
+	s.GET("", controller.FetchSchedule)
 	s.POST("", controller.AddSchedule)
 	s.PUT("/:schedule_id", controller.UpdateSchedule)
 	s.DELETE("/:schedule_id", controller.DeleteSchedule)
