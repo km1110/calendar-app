470758d46ddf3641494fd107452d1dc81bfff25e
kohki miyama <kmx.111010@gmail.com>
2023-10-26 21:58:31 +0900

    feat: :adhesive_bandage: dateの型を修正(#28)

--- backend/golang/app/view/request/todo.go
+++ backend/golang/app/view/request/todo.go
@@ -1,18 +1,20 @@
 package request
 
+import "time"
+
 type CreateTodoRequest struct {
-	Name      string `json:"name"`
-	Date      string `json:"date"`
-	Status    bool   `json:"status"`
-	ProjectId string `json:"project_id"`
-	TagId     string `json:"tag_id"`
+	Name      string    `json:"name"`
+	Date      time.Time `json:"date"`
+	Status    bool      `json:"status"`
+	ProjectId string    `json:"project_id"`
+	TagId     string    `json:"tag_id"`
 }
 
 type UpdateTodoRequest struct {
-	Id        string `json:"id"`
-	Name      string `json:"name"`
-	Date      string `json:"date"`
-	Status    bool   `json:"status"`
-	ProjectId string `json:"project_id"`
-	TagId     string `json:"tag_id"`
+	Id        string    `json:"id"`
+	Name      string    `json:"name"`
+	Date      time.Time `json:"date"`
+	Status    bool      `json:"status"`
+	ProjectId string    `json:"project_id"`
+	TagId     string    `json:"tag_id"`
 }
