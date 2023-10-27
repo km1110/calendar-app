e8741717540d7bc6c3edfecb9087f19b4112d8d7
kohki miyama <kmx.111010@gmail.com>
2023-10-27 15:34:37 +0900

    feat: :sparkles: todo追加のController部分の実装(#28)

--- backend/golang/app/controller/todo.go
+++ backend/golang/app/controller/todo.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/km1110/calendar-app/backend/golang/model"
+	"github.com/km1110/calendar-app/backend/golang/view/request"
 )
 
 func FetchTodo(c *gin.Context) {
@@ -24,4 +25,36 @@ func FetchTodo(c *gin.Context) {
 	}
 
 	c.JSON(http.StatusOK, todos)
+}
+
+func CreateTodo(c *gin.Context) {
+	var req request.CreateTodoRequest
+
+	if err := c.Bind(&req); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	firebaseUID, exists := c.Get("firebaseUID")
+	if !exists {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "firebaseUID not provided"})
+		return
+	}
+
+	um := model.NewUserModel()
+	userID, err := um.GetUser(c.Request.Context(), firebaseUID.(string))
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	tm := model.NewTodoModel()
+	res, err := tm.AddTodos(c, userID, req)
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, res)
 }
