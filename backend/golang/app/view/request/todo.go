05e88ac09c7fad7c723b281cac6e7f946bfb7d2f
kohki miyama <kmx.111010@gmail.com>
2023-10-27 15:36:38 +0900

    feat: :sparkles: tag, projectの型変更(#28)

--- backend/golang/app/view/request/todo.go
+++ backend/golang/app/view/request/todo.go
@@ -3,18 +3,18 @@ package request
 import "time"
 
 type CreateTodoRequest struct {
-	Name      string    `json:"name"`
-	Date      time.Time `json:"date"`
-	Status    bool      `json:"status"`
-	ProjectId string    `json:"project_id"`
-	TagId     string    `json:"tag_id"`
+	Name    string         `json:"name"`
+	Date    time.Time      `json:"date"`
+	Status  bool           `json:"status"`
+	Project ProjectRequest `json:"project"`
+	Tag     TagRequest     `json:"tag"`
 }
 
 type UpdateTodoRequest struct {
-	Id        string    `json:"id"`
-	Name      string    `json:"name"`
-	Date      time.Time `json:"date"`
-	Status    bool      `json:"status"`
-	ProjectId string    `json:"project_id"`
-	TagId     string    `json:"tag_id"`
+	Id      string         `json:"id"`
+	Name    string         `json:"name"`
+	Date    time.Time      `json:"date"`
+	Status  bool           `json:"status"`
+	Project ProjectRequest `json:"project"`
+	Tag     TagRequest     `json:"tag"`
 }
