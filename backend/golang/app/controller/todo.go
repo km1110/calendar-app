636c428fed1380934153528ea32b46a405479afb
kohki miyama <kmx.111010@gmail.com>
2023-10-28 10:28:02 +0900

    feat: :sparkles: todoの更新処理の追加(#28)

--- backend/golang/app/controller/todo.go
+++ backend/golang/app/controller/todo.go
@@ -6,6 +6,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/km1110/calendar-app/backend/golang/model"
 	"github.com/km1110/calendar-app/backend/golang/view/request"
+	"github.com/km1110/calendar-app/backend/golang/view/response"
 )
 
 func FetchTodo(c *gin.Context) {
@@ -50,6 +51,54 @@ func CreateTodo(c *gin.Context) {
 
 	tm := model.NewTodoModel()
 	res, err := tm.AddTodos(c, userID, req)
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, res)
+}
+
+func UpdateTodo(c *gin.Context) {
+	// Requestボディのバリデーション
+	var req request.UpdateTodoRequest
+
+	if err := c.Bind(&req); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	id := c.Param("todo_id")
+
+	tm := model.NewTodoModel()
+	res, err := tm.UpdateTodos(c, response.TodosResponse{
+		Id:      id,
+		Name:    req.Name,
+		Date:    req.Date,
+		Status:  req.Status,
+		Project: response.ProjectResponse(req.Project),
+		Tag:     response.TagResponse(req.Tag),
+	})
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, res)
+}
+
+func UpdateTodoStatus(c *gin.Context) {
+	var req request.UpdateTodoStatusRequest
+
+	if err := c.Bind(&req); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	tm := model.NewTodoModel()
+	res, err := tm.UpdateTodoStatus(c, req)
 
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
