6fdf685b5b849d955c6237b17a665749ba99983b
kohki miyama <kmx.111010@gmail.com>
2023-11-02 11:41:36 +0900

    feat: :sparkles: prot番号を環境変数から読み取れるよう修正(#34)

--- backend/golang/cmd/main.go
+++ backend/golang/cmd/main.go
@@ -1,11 +1,17 @@
 package main
 
 import (
+	"os"
+
 	"github.com/km1110/calendar-app/backend/golang/router"
 )
 
 func main() {
-	if err := router.Router().Run(":8080"); err != nil {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	if err := router.Router().Run(":" + port); err != nil {
 		panic(err)
 	}
 }
