39ea9413fe85a379835e0d25df8328ddb9703be6
kohki miyama <kmx.111010@gmail.com>
2023-11-14 09:03:31 +0900

    refactor: :recycle: スタイルの当て方の変更(#17)

--- frontend/src/components/templates/Schedule.tsx
+++ frontend/src/components/templates/Schedule.tsx
@@ -1,6 +1,16 @@
+import React from "react";
 import { Dispatch, SetStateAction } from "react";
+import Paper from "@mui/material/Paper";
+import { Typography, styled } from "@mui/material";
 
 import { scheduleType } from "@/types/schedule";
+
+const ScheduleStyle = styled(Paper)(({ theme }) => ({
+  marginBottom: "4px",
+  border: "1px solid #e0e0e0",
+  height: "20px",
+  background: "#e0e0e0",
+}));
 
 type Props = {
   schedule: scheduleType;
@@ -10,15 +20,26 @@ type Props = {
 
 export const Schedule = ({ schedule, setSchedule, setShowDialog }: Props) => {
   return (
-    <div
-      className={`bg-neutral-200 p-1 mr-3 text-gray-600 text-sm rounded mb-1 truncate`}
-      onClick={(e) => {
-        e.stopPropagation();
-        setShowDialog(true);
-        setSchedule(schedule);
-      }}
-    >
-      {schedule.title}
-    </div>
+    <ScheduleStyle>
+      <Paper
+        elevation={0}
+        sx={{ background: "#e0e0e0" }}
+        onClick={(e) => {
+          e.stopPropagation();
+          setShowDialog(true);
+          setSchedule(schedule);
+        }}
+      >
+        <Typography
+          sx={{
+            fontSize: "12px",
+            textAlign: "left",
+            marginLeft: "10px",
+          }}
+        >
+          {schedule.title}
+        </Typography>
+      </Paper>
+    </ScheduleStyle>
   );
 };
