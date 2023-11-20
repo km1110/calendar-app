54dec03fc714e1998a2b0fa79978769b5493d811
kohki miyama <kmx.111010@gmail.com>
2023-11-20 20:08:51 +0900

    feat: :sparkles: 日記表示のフォームの作成 #72

---
+++ frontend/src/components/parts/DisplayDiary.tsx
@@ -0,0 +1,53 @@
+import { Dispatch, SetStateAction } from "react";
+
+import { Box, Button, Typography } from "@mui/material";
+
+import { diaryType } from "@/types/diary";
+
+type Props = {
+  diary: diaryType;
+  setDiary: Dispatch<SetStateAction<diaryType>>;
+  setIsEdit: (isEdit: boolean) => void;
+};
+
+export const DisplayDiary = ({ diary, setIsEdit }: Props) => {
+  return (
+    <Box
+      sx={{
+        width: "100%",
+        height: "100%",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+      }}
+    >
+      <Typography
+        sx={{
+          width: "400px",
+          height: "10%",
+          borderBottom: "1px solid #ccc",
+          backgroundColor: "#fff",
+          // padding: "10px",
+          marginBottom: "10px",
+        }}
+      >
+        {diary.title}
+      </Typography>
+      <Box
+        sx={{
+          width: "400px",
+          height: "70%",
+          border: "1px solid #ccc",
+          borderRadius: "5px",
+          backgroundColor: "#fff",
+          whiteSpace: "pre-wrap",
+          overflowWrap: "break-word",
+          padding: "10px",
+        }}
+      >
+        {diary.content}
+      </Box>
+      <Button onClick={() => setIsEdit(true)}>編集</Button>
+    </Box>
+  );
+};
