3ea0ac770bf57330b90013c0cd1a45961537b290
kohki miyama <kmx.111010@gmail.com>
2023-11-28 21:12:42 +0900

    feat: :zap: FirebaseUIDを取得する関数の追加 #72

--- backend/golang/utils/id.go
+++ backend/golang/utils/id.go
@@ -1,11 +1,22 @@
 package utils
 
 import (
+	"fmt"
 	"math/rand"
 	"time"
 
+	"github.com/gin-gonic/gin"
 	"github.com/oklog/ulid"
 )
+
+func GetFirebaseUID(c *gin.Context) (string, error) {
+	firebaseUID, exists := c.Get("firebaseUID")
+	if !exists {
+		return "", fmt.Errorf("firebaseUID not provided")
+	}
+
+	return firebaseUID.(string), nil
+}
 
 func GenerateId() string {
 	t := time.Now()
