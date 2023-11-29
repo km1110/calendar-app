77f4c1f4b063be3263aee876c0f435ea14396196
kohki miyama <kmx.111010@gmail.com>
2023-11-30 08:51:17 +0900

    feat: :zap: 追加、変更、削除の処理追加 #72

--- backend/golang/controller/diary.go
+++ backend/golang/controller/diary.go
@@ -6,6 +6,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/km1110/calendar-app/backend/golang/model"
 	"github.com/km1110/calendar-app/backend/golang/utils"
+	"github.com/km1110/calendar-app/backend/golang/view/request"
 )
 
 func FetchDiary(c *gin.Context) {
@@ -31,4 +32,71 @@ func FetchDiary(c *gin.Context) {
 	}
 
 	c.JSON(http.StatusOK, diarys)
+}
+
+func CreateDiary(c *gin.Context) {
+	var req request.Diary
+
+	if err := c.Bind(&req); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	firebaseUID, err := utils.GetFirebaseUID(c)
+	if err != nil {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": err.Error()})
+		return
+	}
+
+	um := model.NewUserModel()
+	useID, err := um.GetUser(firebaseUID)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	dm := model.NewDiaryModel()
+	res, err := dm.AddDiary(c, useID, req)
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, res)
+}
+
+func UpdateDiary(c *gin.Context) {
+	var req request.Diary
+
+	if err := c.Bind(&req); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	id := c.Param("id")
+
+	dm := model.NewDiaryModel()
+	res, err := dm.UpdateDiary(c, id, req)
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, res)
+}
+
+func DeleteDiary(c *gin.Context) {
+	id := c.Param("id")
+
+	dm := model.NewDiaryModel()
+	err := dm.DeleteDiary(c, id)
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{})
 }
