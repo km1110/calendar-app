484aa13cf767ef2624e73bfb7106248eb7ef3efb
kohki miyama <kmx.111010@gmail.com>
2023-12-02 09:26:59 +0900

    feat: :zap: controllerを引数で受け取る #77

--- backend/golang/router/auth.go
+++ backend/golang/router/auth.go
@@ -5,7 +5,7 @@ import (
 	"github.com/km1110/calendar-app/backend/golang/controller"
 )
 
-func initAuthRouter(router *gin.RouterGroup) {
-	router.POST("/signup", controller.CreateUser)
-	router.GET("/signin", controller.GetUser)
+func initAuthRouter(router *gin.RouterGroup, uc controller.UserController) {
+	router.POST("/signup", uc.CreateUser)
+	router.GET("/signin", uc.GetUser)
 }
