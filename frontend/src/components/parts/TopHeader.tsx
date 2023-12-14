cc5864fddf25a6324ffa745da6125803ce014162
kohki miyama <kmx.111010@gmail.com>
2023-12-14 14:39:03 +0900

    トップページのUI作成 #60

---
+++ frontend/src/components/parts/TopHeader.tsx
@@ -0,0 +1,12 @@
+import { Box, Typography } from "@mui/material";
+import React from "react";
+
+export const TopHeader = () => {
+  return (
+    <Box>
+      <Typography sx={{ marginTop: "10px", marginLeft: "20px" }}>
+        TaskCopilot
+      </Typography>
+    </Box>
+  );
+};
