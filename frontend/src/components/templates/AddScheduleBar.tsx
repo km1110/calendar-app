e623e667ee6784239a3ddf585aabb44deadb42da
kohki miyama <kmx.111010@gmail.com>
2023-12-28 22:33:43 +0900

    feat: :zap: 変数とハンドルの追加

--- frontend/src/components/templates/AddScheduleBar.tsx
+++ frontend/src/components/templates/AddScheduleBar.tsx
@@ -1,4 +1,6 @@
-import { Dispatch, SetStateAction, useState } from "react";
+import { useContext, useState } from "react";
+import { MonthContext } from "@/provider/CalendarProvider";
+
 import {
   Box,
   Button,
@@ -12,7 +14,6 @@ import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
 import dayjs from "dayjs";
 import minMax from "dayjs/plugin/minMax";
 import "dayjs/locale/ja";
-import { scheduleType } from "@/types/schedule";
 
 dayjs.extend(minMax);
 dayjs.locale("ja");
@@ -22,78 +23,77 @@ class DateAdapter extends AdapterDateFns {
 }
 
 type Props = {
-  schedule: scheduleType;
-  setSchdule: Dispatch<SetStateAction<scheduleType>>;
   handleSaveSchedule: () => Promise<void>;
 };
 
-export const AddScheduleBar = ({
-  schedule,
-  setSchdule,
-  handleSaveSchedule,
-}: Props) => {
+export const AddScheduleBar = ({ handleSaveSchedule }: Props) => {
   const today = dayjs();
   const [selectedDate, setSelectedDate] = useState<dayjs.Dayjs | null>(today);
+  const [title, setTitle] = useState("");
+  const { daySelected, setDaySelected, schedule, setSchedule } =
+    useContext(MonthContext);
 
   const theme = createTheme(
     {},
     jaJP // x-date-pickers translations
   );
 
-  const onChange = (date: Date | null) => {
+  const onChangeDate = (date: Date | null) => {
     setSelectedDate(dayjs(date));
-    setSchdule({ ...schedule, date: dayjs(date) });
+    setDaySelected(dayjs(date));
+  };
+
+  const onChangeTitle = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTitle(e.target.value);
+    setSchedule({ ...schedule, title: e.target.value });
   };
 
   return (
-    <form onSubmit={handleSaveSchedule}>
-      <Box
-        display="flex"
-        flexDirection="column"
-        alignItems="center"
-        width="320px"
-        marginTop="30px"
-      >
-        <ThemeProvider theme={theme}>
-          <LocalizationProvider
-            dateAdapter={DateAdapter}
-            dateFormats={{ monthAndYear: "yyyy年 MM月", monthShort: "MM月" }}
-          >
-            <DateCalendar
-              views={["day"]}
-              value={selectedDate?.toDate()}
-              maxDate={today.toDate()}
-              onChange={onChange}
-              sx={{ width: "100%", transform: "scale(0.8)" }}
-            />
-          </LocalizationProvider>
-        </ThemeProvider>
-        <TextField
-          size="small"
-          inputProps={{ style: { fontSize: 18 } }}
-          placeholder="予定"
-          value={schedule.title}
-          onChange={(e) => setSchdule({ ...schedule, title: e.target.value })}
-        />
-        <Button
-          size="small"
-          variant="outlined"
-          type="submit"
-          style={{
-            borderColor: "black",
-            borderRadius: "20px",
-            background: "#014A8F",
-          }}
-          sx={{
-            color: "white",
-            marginTop: "20px",
-            width: "120px",
-            fontSize: "20px",
-          }}
+    <Box
+      display="flex"
+      flexDirection="column"
+      alignItems="center"
+      width="300px"
+      marginTop="30px"
+    >
+      <ThemeProvider theme={theme}>
+        <LocalizationProvider
+          dateAdapter={DateAdapter}
+          dateFormats={{ monthAndYear: "yyyy年 MM月", monthShort: "MM月" }}
         >
-          追加
-        </Button>
-      </Box>
-    </form>
+          <DateCalendar
+            views={["day"]}
+            value={selectedDate?.toDate()}
+            onChange={onChangeDate}
+            sx={{ width: "100%", transform: "scale(0.8)" }}
+          />
+        </LocalizationProvider>
+      </ThemeProvider>
+      <TextField
+        size="small"
+        inputProps={{ style: { fontSize: 18 } }}
+        placeholder="予定"
+        value={title}
+        onChange={onChangeTitle}
+      />
+      <Button
+        size="small"
+        variant="outlined"
+        style={{
+          borderColor: "black",
+          borderRadius: "20px",
+          background: "#014A8F",
+        }}
+        sx={{
+          color: "white",
+          marginTop: "20px",
+          width: "120px",
+          fontSize: "20px",
+        }}
+        onClick={handleSaveSchedule}
+      >
+        追加
+      </Button>
+    </Box>
   );
 };
