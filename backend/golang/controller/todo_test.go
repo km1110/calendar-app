ca9dee5ebd8737ffb3d99269ad61587e3a4bcc5e
kohki miyama <kmx.111010@gmail.com>
2024-01-20 22:16:43 +0900

    test: :sparkles: todoに関するテストを作成

---
+++ backend/golang/controller/todo_test.go
@@ -0,0 +1,175 @@
+package controller
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/km1110/calendar-app/backend/golang/model/entities"
+	"github.com/km1110/calendar-app/backend/golang/view/request"
+	"github.com/km1110/calendar-app/backend/golang/view/response"
+	"github.com/stretchr/testify/assert"
+	// 他に必要なパッケージをインポート
+)
+
+func toTime(t string) time.Time {
+	d, _ := time.Parse("2006-01-02", t)
+	return d
+}
+
+// モックのユーザーマネージャーとTODOマネージャーを定義
+type mockUserManager struct{}
+
+func (m *mockUserManager) GetUser(firebaseUID string) (string, error) {
+	// モックのロジックを実装
+	if firebaseUID == "validUID" {
+		return "123456789", nil
+	}
+	return "", fmt.Errorf("invalid firebaseUID")
+}
+
+func (m *mockUserManager) AddUser(ctx context.Context, r entities.SignUp) (entities.SignUp, error) {
+	// モックのロジックを実装
+	if r.Firebase_uid == "validUID" {
+		return entities.SignUp{
+			Id:           "123456789",
+			Firebase_uid: "validUID",
+			Username:     "validUsername",
+			Email:        "validEmail",
+		}, nil
+	}
+	return entities.SignUp{}, fmt.Errorf("invalid firebaseUID")
+}
+
+type mockTodoManager struct{}
+
+// todoのモックデータ
+var test_todo = response.TodosResponse{
+	Id:     "123456789",
+	Name:   "validName",
+	Date:   toTime("2021-01-01"),
+	Status: false,
+	Project: response.ProjectsResponse{
+		Id:    "123456789",
+		Title: "validName",
+	},
+	Tag: response.TagResponse{
+		Id:   "123456789",
+		Name: "validName",
+	},
+}
+
+func (m *mockTodoManager) GetTodos(user_id string) ([]*response.TodosResponse, error) {
+	if user_id == "123456789" {
+		return []*response.TodosResponse{&test_todo}, nil
+	}
+	return nil, fmt.Errorf("invalid firebaseUID")
+}
+
+func (m *mockTodoManager) GetDailyTodos(user_id, start_date, end_date string) ([]*response.TodosResponse, error) {
+	if user_id == "123456789" && start_date == "2021-01-01" && end_date == "2021-01-02" {
+		return []*response.TodosResponse{&test_todo}, nil
+	}
+	return nil, fmt.Errorf("invalid firebaseUID")
+}
+
+func (m *mockTodoManager) GetDateCount(user_id string, start_year string, end_year string) ([]*entities.TodoDateCount, error) {
+	if user_id == "123456789" && start_year == "2021" && end_year == "2022" {
+		return []*entities.TodoDateCount{
+			{
+				Date:  toTime("2021-01-01"),
+				Count: 1,
+			},
+		}, nil
+	}
+	return nil, fmt.Errorf("invalid firebaseUID")
+}
+
+func (m *mockTodoManager) AddTodos(ctx context.Context, user_id string, r request.CreateTodoRequest) (response.TodosResponse, error) {
+	if user_id == "123456789" {
+		return test_todo, nil
+	}
+	return response.TodosResponse{}, fmt.Errorf("invalid firebaseUID")
+}
+
+func (m *mockTodoManager) UpdateTodos(ctx context.Context, r response.TodosResponse) (response.TodosResponse, error) {
+	if r.Id == "123456789" {
+		return test_todo, nil
+	}
+	return response.TodosResponse{}, fmt.Errorf("invalid firebaseUID")
+}
+
+func (m *mockTodoManager) UpdateTodoStatus(ctx context.Context, id string, r request.UpdateTodoStatusRequest) (response.TodosResponse, error) {
+	if id == "123456789" {
+		return response.TodosResponse{
+			Id:     "123456789",
+			Status: true,
+		}, nil
+	}
+	return response.TodosResponse{}, fmt.Errorf("invalid firebaseUID")
+}
+
+func (m *mockTodoManager) DeleteTodo(ctx context.Context, id string) error {
+	if id == "123456789" {
+		return nil
+	}
+	return fmt.Errorf("invalid firebaseUID")
+}
+
+func TestFetchTodo(t *testing.T) {
+	// モックのマネージャーを設定
+	mockUM := &mockUserManager{}
+	mockTM := &mockTodoManager{}
+
+	// コントローラーを初期化
+	tc := todoController{um: mockUM, tm: mockTM}
+
+	gin.SetMode(gin.TestMode)
+	r := gin.Default()
+
+	// テストするエンドポイントをセットアップ
+	r.GET("/todos", tc.FetchTodo)
+	r.GET("/todos/day-count", tc.FetchTodoCount)
+	r.POST("/todos", tc.CreateTodo)
+	r.PATCH("/todos/:todo_id", tc.UpdateTodo)
+	r.PATCH("/todos/:todo_id/status", tc.UpdateTodoStatus)
+	r.DELETE("/todos/:todo_id", tc.DeleteTodo)
+
+	// テストケースを定義
+	tests := []struct {
+		description     string
+		firebaseUID     string
+		expectedStatus  int
+		expectedContent string
+	}{
+		{"Valid firebaseUID", "validUID", http.StatusOK, `[{"id":"123456789","name":"validName","date":"2021-01-01T00:00:00Z","status":false,"project":{"id":"123456789","title":"validName"},"tag":{"id":"123456789","name":"validName"}}]`},
+		{"No firebaseUID", "", http.StatusBadRequest, `{"error":"firebaseUID not provided"}`},
+		{"Invalid firebaseUID", "invalidUID", http.StatusBadRequest, `{"error":"invalid firebaseUID"}`},
+	}
+
+	for _, test := range tests {
+		t.Run(test.description, func(t *testing.T) {
+			req, _ := http.NewRequest("GET", "/todos", nil)
+			resp := httptest.NewRecorder()
+			c, _ := gin.CreateTestContext(resp)
+			c.Request = req
+
+			if test.firebaseUID != "" {
+				// gin.Contextに渡すために、リクエストヘッダーにfirebaseUIDをセット
+				c.Set("firebaseUID", test.firebaseUID)
+			}
+
+			tc.FetchTodo(c)
+
+			assert.Equal(t, test.expectedStatus, resp.Code)
+
+			if resp.Body != nil {
+				assert.Equal(t, test.expectedContent, resp.Body.String())
+			}
+		})
+	}
+}
