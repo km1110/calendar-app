896bfacb94316688de62b4e52d1e89c41b86cf59
kohki miyama <kmx.111010@gmail.com>
2024-02-16 17:35:17 +0900

    一時的にクエリのログを出力

--- backend/golang/model/todo.go
+++ backend/golang/model/todo.go
@@ -3,6 +3,7 @@ package model
 import (
 	"context"
 	"database/sql"
+	"fmt"
 	"time"
 
 	"github.com/km1110/calendar-app/backend/golang/model/entities"
@@ -203,6 +204,7 @@ func (tm *todoModel) GetDateCount(user_id string, start_year string, end_year st
 							`
 
 	rows, err := tm.db.Query(getQuery, user_id, start_year, end_year)
+	fmt.Println(getQuery)
 	if err != nil {
 		return nil, err
 	}
