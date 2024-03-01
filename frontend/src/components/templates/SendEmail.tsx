02bd52bad7bd3cb0fa736283d5c2b95c270d620b
kohki miyama <kmx.111010@gmail.com>
2024-03-01 18:08:45 +0900

    feat: :zap: Email送信処理の追加

--- frontend/src/components/templates/SendEmail.tsx
+++ frontend/src/components/templates/SendEmail.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import { SendEmailForm } from "../parts/SendEmailForm";
+import { getAuth, sendPasswordResetEmail } from "firebase/auth";
+import { app } from "@/libs/firebase";
 
 export const SendEmail = () => {
   const [signinData, setSigninData] = React.useState({
     email: "",
   });
+
+  const auth = getAuth(app);
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
@@ -16,6 +20,13 @@ export const SendEmail = () => {
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    sendPasswordResetEmail(auth, signinData.email)
+      .then(() => {
+        alert("メールを送信しました");
+      })
+      .catch((error) => {
+        alert("メールの送信に失敗しました");
+      });
   };
 
   return (
